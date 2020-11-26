44bc24507099ed9d2c16e8b3b4ef3d01228e4c95
Carla-Lima <carlacarvalho93@hotmail.com>
2020-11-26 11:53:29 -0300

    Alteração repositório

---
+++ blogpessoaldeploy 1.0/blogpessoal-api/src/main/java/org/generation/blogPessoal/repository/UsuarioRepository.java
@@ -0,0 +1,16 @@
+package org.generation.blogPessoal.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import org.generation.blogPessoal.model.Usuario;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+	public List<Usuario> findAllByNomeContainingIgnoreCase(String nome);
+
+	public Optional<Usuario> findByUsuarioAndSenha(String usuario, String senha);
+
+	public Optional<Usuario> findByUsuario(String usuario);
+}
